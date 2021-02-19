bffc481bc0f19d521f6b5005d100e8ec40d81dcd
aashika1234 <aashrikadhakal@gmail.com>
2021-02-20 00:52:30 +0545

    Vuforia SDK aaded and heart model Implemented

---
+++ Assets/Scripts/rotationbehav.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationbehav : MonoBehaviour
+{
+    Vector3 movement;
+    public int x, y, z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector3(x, y, z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(movement * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/vbscript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class vbscript : MonoBehaviour
+{
+    public GameObject idleheart, animatedheart, videoplane;
+    VirtualButtonBehaviour[] awa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        awa = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < awa.Length; ++i)
+        {
+            awa[i].RegisterOnButtonPressed(OnButtonPressed);
+            awa[i].RegisterOnButtonReleased(OnButtonReleased);
+        }
+        idleheart.SetActive(false);
+        animatedheart.SetActive(false);
+        videoplane.SetActive(false);
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if (vb.VirtualButtonName == "previewvb")
+        {
+            UnityEngine.Debug.Log("previewvb Button pressed");
+            idleheart.SetActive(true);
+            animatedheart.SetActive(false);
+            videoplane.SetActive(false);
+        }
+        else if (vb.VirtualButtonName == "animationvb")
+        {
+            UnityEngine.Debug.Log("animationvb Button pressed");
+            idleheart.SetActive(false);
+            animatedheart.SetActive(true);
+            videoplane.SetActive(false);
+        }
+        else if (vb.VirtualButtonName == "videovb")
+        {
+            UnityEngine.Debug.Log("Traveller Button pressed");
+            idleheart.SetActive(false);
+            animatedheart.SetActive(false);
+            videoplane.SetActive(true);
+        }
+        else
+        {
+            throw new UnityException(vb.VirtualButtonName + "Virtual Button not supported");
+        }
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        UnityEngine.Debug.Log(vb.VirtualButtonName + " released");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
